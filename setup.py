702bdf4266541515323ec9f84966d6ee0240d82a
wangchaoyang <wangchaoyang@joinquant.com>
2018-04-19 16:09:30 +0800

    1.3.2 修复了pip10安装错误的bug

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 # coding=utf-8
-
 import sys
 from os.path import dirname, join
-from pip.req import parse_requirements
+
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
 
 from setuptools import (
     find_packages,
@@ -20,7 +26,7 @@ requirements = [str(ir.req) for ir in parse_requirements("requirements.txt", ses
 setup(
     name="jqdatasdk",
     version=version,
-    description="jqdatasdk<SDK of fetch finance data from JoinQuant>",
+    description="jqdatasdk<easy utility for getting financial market data of China>",
     packages=["jqdatasdk", "jqdatasdk.macro_tables"],
     author="JoinQuant",
     author_email="xlx@joinquant.com",
