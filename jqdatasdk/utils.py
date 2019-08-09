8f238791ff2a20bdebe12e6fb324783cd2c90caf
sunzhenyu <sunzhenyu@joinquant.com>
2019-08-09 19:52:15 +0800

    add get_mac_address

--- jqdatasdk/utils.py
+++ jqdatasdk/utils.py
@@ -221,3 +221,7 @@ def assert_auth(func):
             return func(*args, **kwargs)
     return _wrapper
 
+def get_mac_address():
+    import uuid
+    mac = uuid.UUID(int=uuid.getnode()).hex[-12:].upper()
+    return '%s:%s:%s:%s:%s:%s' % (mac[0:2], mac[2:4], mac[4:6], mac[6:8],mac[8:10], mac[10:])
